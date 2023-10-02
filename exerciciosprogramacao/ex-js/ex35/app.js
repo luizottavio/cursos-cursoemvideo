798f332b974381f4a9fb793c6aa3ecd5a96c6649
luizottavio <102751708+luizottavio@users.noreply.github.com>
2023-10-02 14:22:49 -0300

    Update

--- exerciciosprogramacao/ex-js/ex35/app.js
+++ exerciciosprogramacao/ex-js/ex35/app.js
@@ -1,4 +1,21 @@
-const horas = document.querySelector('.horas')
-const minutos = document.querySelector('.minutos')
-const seguntos = document.querySelector('.segundos')
-const hors = newData()
+const div = document.querySelector('.div')
+
+const mHoras = (() => {
+    const data = new Date()
+    const h = data.getHours()
+    const m = data.getMinutes()
+    const s = data.getSeconds()
+
+    div.textContent = `${h}:`
+    div.textContent += `${m}:`
+    div.textContent += `${s}`
+
+    div.textContent = `${String(h).length === 1 ? `0${h}` : h}:
+    ${String(m).length === 1 ? `0${m}` : m }:
+    ${String(s).length === 1 ? `0${s}` : s}
+`
+})
+
+setInterval(mHoras, 1000)
+
+mHoras()
